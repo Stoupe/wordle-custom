bdfff5b46a6c33defe38a09dbcde5c7790639b7d
Henry Stoupe <h.stoupe@gmail.com>
2022-05-22 00:23:15 +1200

    Remove unused method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,36 +1,4 @@
-import { LetterBoxState } from '../components/LetterBox/LetterBox';
 import { Tile } from '../models/gameState';
-
-export const calculateColor = (
-  guessedLetter: string,
-  index: number,
-  guessedWord: string,
-  correctWord: string
-): LetterBoxState => {
-  // Letter not in the correct word at all in correct word
-  if (!correctWord.split('').includes(guessedLetter)) {
-    return 'default';
-  }
-
-  if (correctWord.includes(guessedLetter)) {
-    // Correct letter in the correct position
-    if (correctWord[index] === guessedLetter) {
-      return 'green';
-    }
-
-    // Correct letter which hasn't been guessed yet, but in the wrong position
-    const notYetGuessedLetters = correctWord
-      .split('')
-      .filter((letter, i) => guessedWord[i] !== letter);
-
-    if (notYetGuessedLetters.includes(guessedLetter)) {
-      return 'orange';
-    }
-  }
-
-  // Letter either not in the word or already placed elsewhere correctly
-  return 'default';
-};
 
 /**
  * Pick a random string from an array of strings
