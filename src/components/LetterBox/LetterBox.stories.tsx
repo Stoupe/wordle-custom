ab8eeec992d1f723295cc25f1b7cc86d55582bdf
Henry Stoupe <h.stoupe@gmail.com>
2022-05-22 00:02:44 +1200

    Add small variant to LetterBox

--- src/components/LetterBox/LetterBox.stories.tsx
+++ src/components/LetterBox/LetterBox.stories.tsx
@@ -6,19 +6,34 @@ export default {
   component: LetterBox,
   title: 'Pattern Library/Letter Box',
   args: {
-    tile: { letter: 'a', state: 'unknown' }
+    tile: { letter: 'a', state: 'unknown' },
+    variant: 'large'
   }
 } as ComponentMeta<typeof LetterBox>;
 
 export const Default: ComponentStoryObj<typeof LetterBox> = {};
 
-export const AllCases: ComponentStoryObj<typeof LetterBox> = {
+export const Large: ComponentStoryObj<typeof LetterBox> = {
   render: () => (
     <Group>
       <LetterBox tile={{ letter: 'a', state: 'correct' }} />
       <LetterBox tile={{ letter: 'b', state: 'incorrect' }} />
       <LetterBox tile={{ letter: 'c', state: 'wrongLocation' }} />
       <LetterBox tile={{ letter: 'd', state: 'unknown' }} />
+    </Group>
+  ),
+  parameters: {
+    controls: { disable: true }
+  }
+};
+
+export const Small: ComponentStoryObj<typeof LetterBox> = {
+  render: () => (
+    <Group>
+      <LetterBox tile={{ letter: 'a', state: 'correct' }} variant="small" />
+      <LetterBox tile={{ letter: 'b', state: 'incorrect' }} variant="small" />
+      <LetterBox tile={{ letter: 'c', state: 'wrongLocation' }} variant="small" />
+      <LetterBox tile={{ letter: 'd', state: 'unknown' }} variant="small" />
     </Group>
   ),
   parameters: {
--- src/components/LetterBox/LetterBox.tsx
+++ src/components/LetterBox/LetterBox.tsx
@@ -1,40 +1,54 @@
-import { Box, Text, Transition } from '@mantine/core';
+import { Box, CSSObject, MantineTheme, Text, Transition } from '@mantine/core';
 import { Tile } from '../../models/gameState';
 
-export type LetterBoxState = 'default' | 'green' | 'orange';
+interface LetterBoxProps {
+  tile: Tile | undefined;
+  variant?: 'large' | 'small';
+}
 
 /**
  * Represents a single letter box in the game board.
  * If tile is undefined, a blank letter box is rendered.
  * @returns
  */
-const LetterBox = ({ tile }: { tile: Tile | undefined }) => {
+const LetterBox = ({ tile, variant = 'large' }: LetterBoxProps) => {
+  const useLetterBoxStyles = (theme: MantineTheme): CSSObject => ({
+    background:
+      tile?.state === 'correct'
+        ? theme.colors.green
+        : tile?.state === 'wrongLocation'
+        ? theme.colors.orange['6']
+        : tile?.state === 'incorrect'
+        ? theme.fn.darken(theme.colors.gray['7'], 0.15)
+        : theme.colors.gray['7'],
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '0.5rem',
+
+    ...(variant === 'large'
+      ? {
+          width: '2rem',
+          height: '2rem',
+          padding: '2rem'
+        }
+      : variant === 'small'
+      ? {
+          width: '1rem',
+          height: '1rem',
+          padding: '1rem'
+        }
+      : {})
+  });
+
   return (
-    <Box
-      sx={(theme) => ({
-        padding: '2rem',
-        background:
-          tile?.state === 'correct'
-            ? theme.colors.green
-            : tile?.state === 'wrongLocation'
-            ? theme.colors.orange['6']
-            : tile?.state === 'incorrect'
-            ? theme.fn.darken(theme.colors.gray['7'], 0.15)
-            : theme.colors.gray['7'],
-        width: '2rem',
-        height: '2rem',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: '0.5rem'
-      })}
-    >
+    <Box sx={(theme) => useLetterBoxStyles(theme)}>
       <Transition transition={'pop'} duration={200} mounted={tile?.letter !== undefined}>
         {(style) => (
           <Text
             style={style}
             sx={{
-              fontSize: tile ? '2rem' : '0'
+              fontSize: !tile ? '0' : variant === 'large' ? '2rem' : '1rem'
               // transition: "font-size 0.1s",
             }}
             weight={'bold'}
